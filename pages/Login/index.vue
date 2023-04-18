f66688b560ab9558d0e6f7bfddca5b279cf81498
wangyongalive <jsonwang0102@163.com>
2023-04-18 22:36:01 +0800

    登录页面

--- pages/Login/index.vue
+++ pages/Login/index.vue
@@ -1,9 +1,42 @@
 <template>
-	<view class="login-container">
+	<view class="app">
 		<transition name="plus-icon">
-			<image v-if="loading" src="../../static/jd.jpeg"></image>
+			<image class="log" v-if="loading" src="../../static/jd.jpeg"></image>
 		</transition>
-		<view>11111111111</view>
+
+		<view class="top-container">
+			<u-icon name="close"></u-icon>
+			<text>帮助</text>
+		</view>
+
+
+		<view class="log-container">
+			<image class="log2" src="../../static/jd-icon.png"></image>
+		</view>
+
+		<view class="login-container">
+			<u--input placeholder="国家/地区" shape="circle" border="surround" disabled>
+				<u--text text="+86" slot="suffix" margin="0 3px 0 0" type="tips"></u--text>
+			</u--input>
+			<u--input class="resetInput" placeholder="请输入手机号码" shape="circle" border="surround" v-model="value"
+				@change="change"></u--input>
+		</view>
+		<view class="rule-container">
+			<u-checkbox-group v-model="checkboxValue1" placement="column" @change="checkboxChange">
+				<u-checkbox shape="circle" :customStyle="{ marginBottom: '8px' }" v-for="(item, index) in checkboxList1"
+					:key="index" :label="item.name" :name="item.name">
+				</u-checkbox>
+			</u-checkbox-group>
+			<view>
+				未注册的手机号验证后将自动创建京东账号,登录即代表您已同意
+				<span style="color:dodgerblue">《京东隐式政策》</span>
+			</view>
+
+		</view>
+		<view style="padding: 20px;">
+			<u-button type="error" text="获取验证码" shape="circle" color="linear-gradient(to right, #f10000, #ff4f18)"
+				:disabled="!checkboxValue1.length" @click="handleCapture"></u-button>
+		</view>
 
 	</view>
 </template>
@@ -13,7 +46,15 @@ import { textFn } from '../../api/index.js'
 export default {
 	data() {
 		return {
-			loading: true
+			loading: false,
+			value: '',
+			checkboxValue1: [],
+			// 基本案列数据
+			checkboxList1: [{
+				name: '',
+				disabled: false
+			}
+			],
 		}
 	},
 	onLoad() {
@@ -26,47 +67,37 @@ export default {
 		textFn()
 	},
 	methods: {
-
+		change(e) {
+			console.log('change', e);
+		},
+		handleCapture() {
+			uni.navigateTo({
+				url: `/pages/VerificationCode/index?code=${1}`
+			})
+		}
 	}
 }
 </script>
 
 <style lang="scss">
-/* .content {
-	display: flex;
-	flex-direction: column;
-	align-items: center;
-	justify-content: center;
-}
+$padding: 40rpx;
 
-.logo {
-	height: 200rpx;
-	width: 200rpx;
-	margin-top: 200rpx;
-	margin-left: auto;
-	margin-right: auto;
-	margin-bottom: 50rpx;
-}
-
-.text-area {
-	display: flex;
-	justify-content: center;
-}
-
-.title {
-	font-size: 36rpx;
-	color: #8f8f94;
-} */
-
-.login-container {
+.app {
 	height: 100vh;
 	width: 100vw;
+	padding: $padding;
+	box-sizing: border-box;
 
-	image {
+	.log {
 		position: fixed;
 		z-index: 100;
 		width: 100%;
 		height: 100%;
+	}
+
+	.log2 {
+		width: 100px;
+		height: 100px;
 	}
 
 	.plus-icon-enter-active {
@@ -85,6 +116,25 @@ export default {
 		opacity: 0;
 	}
 
+	.log-container {
+		padding: $padding;
+		text-align: center;
+	}
 
+	.top-container {
+		display: flex;
+		justify-content: space-between;
+	}
+
+	.rule-container {
+		padding-top: $padding;
+		display: flex;
+	}
+}
+</style>
+<style>
+.resetInput.u-input {
+	margin-top: 10px;
+	background-color: rgb(245, 247, 250);
 }
 </style>
--- pages/VerificationCode/index.vue
+++ pages/VerificationCode/index.vue
@@ -5,8 +5,7 @@
             <view class="">请输入验证码</view>
             <view><text class="text-info">已发送至</text><text>+86 138****6251</text></view>
             <u-code-input @change="change" @finish="finish"></u-code-input>
-            <u-button shape="circle" color="linear-gradient(to right, #f10000, #ff4f18"
-                text="登录"></u-button>
+            <u-button shape="circle" color="linear-gradient(to right, #f10000, #ff4f18)" text="登录"></u-button>
         </view>
     </view>
 </template>
@@ -50,14 +49,17 @@ page {
         flex-direction: column;
         align-items: center;
         justify-content: space-between;
+
         img {
             width: 100PX;
             height: 100PX;
         }
+
         .text-info {
             color: #8f9ca2;
             font-size: 14PX;
         }
     }
 
-}</style>
+}
+</style>
